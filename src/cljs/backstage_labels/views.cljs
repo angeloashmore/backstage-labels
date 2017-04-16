daaa6cb4c33682126a38d304bcff8d342ae6108c
Angelo Ashmore <angeloashmore@me.com>
2017-04-15 16:26:31 -1000

    Remove app component

--- src/cljs/backstage_labels/views.cljs
+++ src/cljs/backstage_labels/views.cljs
@@ -1,17 +1,22 @@
 (ns backstage-labels.views
   (:require [re-frame.core :as re-frame :refer [subscribe]]
+            [cljs-css-modules.macro :refer-macros [defstyle]]
             [backstage-labels.panels.home :as home]
-            [backstage-labels.panels.about :as about]
-            [backstage-labels.components.app :as app]))
+            [backstage-labels.panels.about :as about]))
 
-;; ---------------------------
+(defstyle style
+  [".main" {:display "flex"
+            :font-family "BlinkMacSystemFont"
+            :height "100%"
+            :user-select "none"}])
 
 (defmulti panels identity)
 (defmethod panels :home-panel [] [home/main])
 (defmethod panels :about-panel [] [about/main])
 (defmethod panels :default [] [:div])
 
-(defn main-panel []
+(defn main-panel
+  []
   (let [active-panel (re-frame/subscribe [:active-panel])]
     (fn []
-      [app/main {:panel (panels @active-panel)}])))
+      [:div {:class-name (:main style)} (panels @active-panel)])))
