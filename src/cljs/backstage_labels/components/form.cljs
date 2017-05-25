5e55b993c47d5b88d2b2ea4078f5e2b95d011ed6
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2017-05-25 13:59:16 -1000

    Extract label and select components

---
+++ src/cljs/backstage_labels/components/form.cljs
@@ -0,0 +1,41 @@
+(ns backstage-labels.components.form
+  (:require [cljs-css-modules.macro :refer-macros [defstyle]]
+            [garden.units :as units]
+            [garden.color :as color]
+            [backstage-labels.config :as config]))
+
+(defstyle style
+  [".select" {:background-color (:background--field config/theme)
+              :background-image "url(./images/dropdown.svg)";
+              :background-position [["right" (units/px 8) "center"]]
+              :background-repeat "no-repeat"
+              :border 0
+              :border-radius (units/px 4)
+              :box-shadow [[0 0 0 (units/px 0.5) (color/rgba 0 0 0 0.07)]
+                           [0 (units/px 0.5) (units/px 0.5) (color/rgba 0 0 0 0.2)]]
+              :font-size (units/px 12)
+              :padding-bottom (units/px 4)
+              :padding-left (units/px 8)
+              :padding-right (units/px 8)
+              :padding-top (units/px 4)
+              :outline 0}]
+
+  [".label" {:color (:text--secondary config/theme)
+             :font-size (units/px 12)
+             :margin-right (units/px 10)}])
+
+(defn select
+  "Styled select component."
+  [{:keys [class on-change]} & children]
+  [:select {:class (garden.util/space-join [(:select style) class])
+            :on-change on-change}
+   children])
+
+(defn label
+  "Styled label component."
+  ([{:keys [class]} & children]
+   [:label {:class (garden.util/space-join [(:label style) class])}
+    children])
+  ([& children]
+   [:label {:class (:label style)}
+    children]))
--- src/cljs/backstage_labels/components/print_options_bar.cljs
+++ src/cljs/backstage_labels/components/print_options_bar.cljs
@@ -3,36 +3,17 @@
             [cljs-css-modules.macro :refer-macros [defstyle]]
             [garden.units :as units]
             [garden.color :as color]
-            [backstage-labels.config :as config]))
+            [backstage-labels.config :as config]
+            [backstage-labels.components.form :as form]))
 
 (defstyle style
   [".container" {:align-items "center"
                  :background-color (:background--secondary config/theme)
                  :box-shadow [["inset" 0 (units/px -0.5) 0 (color/rgba 0 0 0 0.15)]]
                  :display "flex"
-                 :flex "none"
                  :padding [[(units/px 10) (units/px 15)]]}]
 
-  [".label" {:color (:text--secondary config/theme)
-             :font-size (units/px 12)
-             :margin-right (units/px 10)}]
-
-  [".select" {:background-color (:background--field config/theme)
-              :background-image "url(./images/dropdown.svg)";
-              :background-position [["right" (units/px 8) "center"]]
-              :background-repeat "no-repeat"
-              :border 0
-              :border-radius (units/px 4)
-              :box-shadow [[0 0 0 (units/px 0.5) (color/rgba 0 0 0 0.07)]
-                           [0 (units/px 0.5) (units/px 0.5) (color/rgba 0 0 0 0.2)]]
-              :display "flex"
-              :flex "1 1 auto"
-              :font-size (units/px 12)
-              :padding-bottom (units/px 4)
-              :padding-left (units/px 8)
-              :padding-right (units/px 8)
-              :padding-top (units/px 4)
-              :outline 0}])
+  [".select" {:flex "1 1 auto"}])
 
 (defn option
   "Option for select element."
@@ -46,9 +27,6 @@
         template                  (re-frame/subscribe [:print-option-template])
         set-print-option-template #(re-frame/dispatch [:set-print-option-template %])]
     [:div {:class (:container style)}
-     ;; Label
-     [:div {:class (:label style)} "Template:"]
-
-     ;; Select
-     [:select {:class (:select style)}
+     [form/label "Template:"]
+     [form/select {:class (:select style)}
       (map option templates)]]))
