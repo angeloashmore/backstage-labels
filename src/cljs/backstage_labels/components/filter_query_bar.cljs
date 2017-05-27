5149efb048f437747d10f0b7b02f1f60bdf0e560
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2017-05-27 10:31:37 -1000

    Fix query bar rendering with non-blank value

--- src/cljs/backstage_labels/components/filter_query_bar.cljs
+++ src/cljs/backstage_labels/components/filter_query_bar.cljs
@@ -6,20 +6,24 @@
             [backstage-labels.config :as config]))
 
 (defstyle style
-  [".container" {:background-color (:background--secondary config/theme)
-                 :background-image "url(./images/search.svg)"
-                 :background-position [[(units/px+ (units/px 1.5) (units/px 16)) "center"]]
-                 :background-repeat "no-repeat"
-                 :border-weight 0
-                 :box-shadow [["inset" 0 (units/px -0.5) 0 (color/rgba 0 0 0 0.15)]
-                              ["inset" (units/px -0.5) 0 0 (color/rgba 0 0 0 0.15)]]
-                 :font-size (units/px 24)
-                 :font-weight 300
-                 :letter-spacing (units/px 1)
-                 :margin 0
-                 :padding 0
-                 :padding-left (units/px 50)
-                 :outline 0}])
+  [".container" {:background-color (:background--secondary config/theme)}]
+
+  [".input" {:background-color "transparent"
+             :background-image "url(./images/search.svg)"
+             :background-position [[(units/px+ (units/px 1.5) (units/px 16)) "center"]]
+             :background-repeat "no-repeat"
+             :border 0
+             :box-shadow [["inset" 0 (units/px -0.5) 0 (color/rgba 0 0 0 0.15)]
+                          ["inset" (units/px -0.5) 0 0 (color/rgba 0 0 0 0.15)]]
+             :font-size (units/px 24)
+             :font-weight 300
+             :height (units/percent 100)
+             :letter-spacing (units/px 1)
+             :margin 0
+             :padding 0
+             :padding-left (units/px 50)
+             :outline 0
+             :width (units/percent 100)}])
 
 (defn main
   "Displays query filter input."
@@ -27,8 +31,9 @@
   (let [query     (re-frame/subscribe [:filter-query])
         resolve   #(-> % str clojure.string/trim)
         set-query #(re-frame/dispatch [:set-filter-query (resolve %)])]
-    [:input {:id        "filter-query"
-             :class     (:container style)
-             :type      "text"
-             :value     @query
-             :on-change #(set-query (-> % .-target .-value))}]))
+    [:div {:class (:container style)}
+     [:input {:id        "filter-query"
+              :class     (:input style)
+              :type      "text"
+              :value     @query
+              :on-change #(set-query (-> % .-target .-value))}]]))
